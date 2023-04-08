013eebeed5df9adce11cda38cb5f7f878c557b5f
The Odd Institute <amir@Thes-Mac-Studio.local>
2023-04-08 13:57:54 -0700

    up

---
+++ Power.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// Define a new infix operator **
+infix operator **: MultiplicationPrecedence
+
+//** operator for types conforming Numeric and SignedNumeric
+func ** <T: Numeric & SignedNumeric>(lhs: T, rhs: Int) -> T
+{
+    var res: T = 1
+    if rhs > 0
+    {
+        for _ in 0..<rhs
+        {
+            res *= lhs
+        }
+    }
+    else if rhs == 0
+    {
+        res = 1
+    }
+    else
+    {
+        fatalError("The ** operator is only implemented for positive powers")
+    }
+    
+    return res
+}
+
+let a = 3 ** 3
+
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Swifit-Power
+
+I've always found Swift's way of "power" a little annoying. Needed something like the one in Python. Thought might be useful to someone else as well
+#swift #ios
+
+![alt text](https://github.com/The-Odd-Institute/Swifit-Power/Swift**?raw=true)
